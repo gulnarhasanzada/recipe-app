af9d0a04522afc77693c3a7a5d419a1a3fac59f5
gulnarhasanzada <gulnar.hesenzade@gmail.com>
2024-04-18 16:03:27 -0400

    Footer color change

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
       <div style={{ marginTop: '10rem' }}>
         <nav className="justify-content-center bg-dark p-4 fixed-bottom">
-          <p className="text-dark text-center">
+          <p className="text-light text-center">
             Copyright {new Date().getFullYear()}
           </p>
         </nav>
